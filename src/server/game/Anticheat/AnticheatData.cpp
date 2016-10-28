db47ecd863338d9d5c07f12c1f21717587d83b56
Eliminationzx <timurdelimin@sibsau.ru>
2016-10-28 22:37:40 +0700

    Anticheat warning fix

--- src/server/game/Anticheat/AnticheatData.cpp
+++ src/server/game/Anticheat/AnticheatData.cpp
@@ -36,10 +36,10 @@ void AnticheatData::SetLastOpcode(uint32 opcode)
 
 void AnticheatData::SetPosition(float x, float y, float z, float o)
 {
-    lastMovementInfo.pos = { x, y, z, o };
-
-
-
+	lastMovementInfo.pos.m_positionX = x;
+	lastMovementInfo.pos.m_positionY = y;
+	lastMovementInfo.pos.m_positionZ = z;
+	lastMovementInfo.pos.m_orientation = o;
 }
 
 uint32 AnticheatData::GetLastOpcode() const
