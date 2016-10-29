512182e70a882365c8c5219b2fc137b0b18a1619
Eliminationzx <timurdelimin@sibsau.ru>
2016-10-29 21:45:53 +0700

    Test

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -2267,10 +2267,11 @@ void Spell::AddUnitTarget(Unit* target, uint32 effectMask, bool checkIfValid /*=
 			// Client latency calculation
 			if (Player* player = m_caster->ToPlayer())
 			{
-				uint64 m_clientLatency = uint64(player->GetSession()->GetLatency());
-				targetInfo.timeDelay = m_clientLatency > MAX_CLIENT_LATENCY_NORM ? MAX_CLIENT_LATENCY_NORM / 2 : m_clientLatency;
+				//uint64 m_clientLatency = uint64(player->GetSession()->GetLatency());
+				targetInfo.timeDelay = 200LL;/*m_clientLatency > MAX_CLIENT_LATENCY_NORM ? MAX_CLIENT_LATENCY_NORM / 2 : m_clientLatency;*/
 			}
 
+			// Don't set time delay at every time
 			if (m_delayMoment == 0 || m_delayMoment != targetInfo.timeDelay)
 				m_delayMoment = targetInfo.timeDelay;
 		}
